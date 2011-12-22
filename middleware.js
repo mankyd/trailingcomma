3529334b2263aef33e8647e8aa5aaf831fcca79d
dave mankoff <mankyd@gmail.com>
2011-12-22 09:39:46 -0500

    add paypal verification to donation page

--- middleware.js
+++ middleware.js
@@ -1,22 +1,24 @@
 exports.rawBodyParser = function (options){
     return function bodyParser(req, res, next) {
-        if (req.body) return next();
-        req.body = {};
+        if (req.raw_body) return next();
+        req.raw_body = '';
 
         if ('GET' == req.method || 'HEAD' == req.method){
             return next();
         }
 
         var buf = '';
-        req.setEncoding('utf8');
-        req.on('data', function(chunk){ buf += chunk });
+        //req.setEncoding('utf8');
+        req.on('data', function(chunk){ buf += chunk; });
         req.on('end', function(){
             try {
                 req.raw_body = buf;
-                next();
+                //next();
             } catch (err){
                 next(err);
             }
         });
+
+        return next();
     };
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
  * GET home page.
  */
 
+var ipn = require('paypal-ipn');
 var redis = require("redis");
 var rclient = redis.createClient(null,  null, {no_ready_check: true});
 
@@ -12,13 +13,17 @@ rclient.on("error", function (err) {
 
 exports.rclient = rclient;
 
-//rclient.auth('devel');
-
 var find = require('../lib/finder').find;
 var markov = require('../lib/markov');
 
 exports.index = function(req, res){
-    res.render('index.jade', { title: 'Trailing Comma,' })
+    if (!req.session.session_id) {
+        req.session.session_id = markov.generate(markov.BASE_ANALYSIS, 16, 16);
+    }
+    res.render('index.jade', {
+        title: 'Trailing Comma,',
+        session_id: req.session.session_id
+    });
 };
 
 exports.check = function(req, res) {
@@ -44,14 +49,39 @@ exports.check = function(req, res) {
 };
 
 exports.donated = function(req, res) {
-    var api_key = req.session.api_key;
-    if (!api_key) {
-        api_key = markov.generate(markov.BASE_ANALYSIS, 16, 16);
-        req.session.api_key = api_key;
+    var verify = function(num_tries) {
+        ipn.verify(req.body, function(err, msg) {
+            if (err && num_tries > 0) {
+                verify(num_tries-1);
+            }
+            else if (num_tries <= 0) {
+                res.redirect('home');
+            }
+            else {
+                if (!req.session.api_key) {
+                    req.session.api_key = markov.generate(markov.BASE_ANALYSIS, 16, 16);
+                    rclient.sadd('api_keys', req.session.api_key);
+                    res.redirect('/donated');
+                }
+            }
+        });
+    };
+
+    if (req.method == 'POST') {
+        if (req.body.custom == req.session.session_id) {
+            verify(1);
+        }
+        else {
+            res.redirect('home');
+        }
     }
-    rclient.sadd('api_keys', api_key);
-    res.render('donated.jade', {
-        title: 'Thanks for the donation!',
-        api_key: api_key
-    })
+    else if (!req.session.api_key) {
+        res.redirect('home');
+    }
+    else {
+        res.render('donated.jade', {
+            title: 'Thanks for the donation!',
+            api_key: req.session.api_key
+        });
+    }
 };
