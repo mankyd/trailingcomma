24362df154a7e8c1d89e4de41ae74075130eea1a
dave mankoff <mankyd@gmail.com>
2014-03-08 17:01:05 -0500

    Remove donation feature

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -7,7 +6,6 @@ var express = require('express');
 var routes = require('./routes');
 var middleware = require('./middleware');
 var settings = require('./settings');
-var redis_store = require('connect-redis')(express);
 
 var app = module.exports = express.createServer();
 
@@ -18,7 +16,7 @@ app.configure(function(){
     app.use(middleware.rawBodyParser());
     app.use(express.bodyParser());
     app.use(express.cookieParser());
-    app.use(express.session({ secret: settings.session_secret, store: new redis_store}));
+    app.use(express.session({ secret: settings.session_secret}));
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(__dirname + '/static'));
@@ -26,14 +24,12 @@ app.configure(function(){
 
 app.configure('development', function(){
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.enable('paypal_testing');
     app.disable('compressed_static');
     app.disable('tracking');
 });
 
 app.configure('production', function(){
     app.use(express.errorHandler()); 
-    app.disable('paypal_testing');
     app.enable('compressed_static');
     app.enable('tracking');
 });
@@ -41,9 +37,6 @@ app.configure('production', function(){
 app.dynamicHelpers({
     compressed_static: function() {
         return app.enabled('compressed_static');
-    },
-    paypal_testing: function(req, res){
-        return app.enabled('paypal_testing');
     },
     static_url: function(req, res) {
         return function(url) {
@@ -62,10 +55,7 @@ app.dynamicHelpers({
 
 app.get('/', routes.index);
 app.post('/check', routes.check);
-app.get('/donated', routes.donated);
-app.post('/donated', routes.donated);
-
 
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
+console.log("Express server listening on port %d in %s mode", 
+            app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,6 @@
-
 /*
  * GET home page.
  */
-
-var ipn = require('paypal-ipn');
-var redis = require("redis");
-var rclient = redis.createClient(null,  null, {no_ready_check: true});
-
-rclient.on("error", function (err) {
-    console.log("Redis Error " + err);
-});
-
-exports.rclient = rclient;
 
 var find = require('../lib/finder').find;
 var markov = require('../lib/markov');
@@ -27,64 +16,11 @@ exports.index = function(req, res){
 };
 
 exports.check = function(req, res) {
-    if (!req.param('key')) {
-        res.statusCode = 403;
-        res.json('No API key specified');
-    }
-    else if (!req.raw_body) {
+    if (!req.raw_body) {
         res.statusCode = 400;
         res.json('No code error')
     }
     else {
-        rclient.sismember('api_keys', req.param('key'), function (err, reply) {
-            if (!reply) {
-                res.statusCode = 403;
-                res.json('Invalid API key');
-            }
-            else {
-                res.json(find(req.raw_body));
-            }
-        });
-    }
-};
-
-exports.donated = function(req, res) {
-    var verify = function(num_tries) {
-        if (num_tries <= 0) {
-            res.redirect('home');
-        }
-        if (!req.app.enabled('paypal_testing') && req.body.test_ipn === '1') {
-            res.redirect('home');
-        }
-        ipn.verify(req.body, function(err, msg) {
-            if (err && num_tries > 0) {
-                verify(num_tries-1);
-            }
-            else {
-                if (!req.session.api_key) {
-                    req.session.api_key = markov.generate(markov.BASE_ANALYSIS, 16, 16);
-                    rclient.sadd('api_keys', req.session.api_key);
-                    res.redirect('/donated');
-                }
-            }
-        });
-    };
-
-    if (req.method == 'POST') {
-        if (req.body.custom == req.session.session_id) {
-            verify(1);
-        }
-        else {
-            res.redirect('home');
-        }
-    }
-    else if (!req.session.api_key) {
-        res.redirect('home');
-    }
-    else {
-        res.render('donated.jade', {
-            title: 'Thanks for the donation!',
-            api_key: req.session.api_key
-        });
+      res.json(find(req.raw_body));
     }
 };
